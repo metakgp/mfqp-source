20ee9bc8481f0e2e94e7988cd9a6b5af4118f420
Siddharth Kannan <siddharthkannan@tutanota.com>
2016-02-21 12:37:32 +0530

    Move the subjects JSON over to the include/ dir
    - grunt-include-replace will automatically template that file into the
      JS file once `grunt build` is run.
    
    Signed-off-by: Siddharth Kannan <siddharthkannan@tutanota.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,35 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-		clean: ['dist'],
+		clean: {
+			  dist: ["dist"],
+				temp: [".tmp"]
+		},
 		copy: {
 			dist: {
 				files: [{
 					expand: true,
-					src: ['index.html','jsmain.js','cssmain.css','dep.html'],
+					src: ['index.html','cssmain.css','dep.html'],
 					dest: 'dist/'
 				}]
+			},
+			templated: {
+				files: [{
+					expand: false,
+					src: ['.tmp/jsmain.js'],
+					dest: 'dist/jsmain.js'
+				}]
+			}
+		},
+		includereplace: {
+			build_js: {
+				options: {
+						// Task-specific options go here.
+				},
+				// Files to perform replacements and includes with
+				src: 'jsmain.js',
+				// Destination directory to copy files to
+				dest: '.tmp/'
 			}
 		},
 				
@@ -20,7 +41,7 @@ module.exports = function(grunt) {
 		}
 	});
 
-    grunt.registerTask('build', ['clean', 'copy:dist']);
+    grunt.registerTask('build', ['clean:dist', 'clean:temp', 'includereplace:build_js', 'copy:dist', 'copy:templated', 'clean:temp']);
 
 	require('load-grunt-tasks')(grunt);
 };
