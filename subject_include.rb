f0cc71644394a92507c53a5746639215998edae0
Siddharth Kannan <siddharthkannan@tutanota.com>
2016-02-24 23:29:27 +0530

    Fix the ruby script, pretty print JSON
    - Nasty mistake found (I wrote it), instead of doing
      obj[i][1] = true I did obj[i] = true, which removed the paper from
      the papers being scanned.

--- subject_include.rb
+++ subject_include.rb
@@ -5,10 +5,14 @@ obj = JSON.parse(File.read("./include/subjects.json"))
 
 for i in ARGV
 	if obj[i].is_a?(Array) && obj[i].length == 2 && obj[i][1] == false
-		obj[i] = true
+		obj[i][1] = true
 		puts "Toggled #{i}"
 	else
-		puts "#{i} is not a part of this JSON or was already marked true"
+		if obj[i][1]
+			puts "#{i} was already true, proceed!"
+		else
+			puts "#{i} is not a part of this JSON, probably last semester?"
+		end
 	end
 end
 
