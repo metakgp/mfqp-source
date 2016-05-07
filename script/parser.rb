97a2810fe74029c452b527eefac0801e3ecffa52
Siddharth Kannan <siddharthkannan@protonmail.com>
2016-05-07 12:35:34 +0530

    more change to the script, works now
    - Still to inspect the output that it is spitting out into the data/ and
      data/pdf/ directories.

--- script/parser.rb
+++ script/parser.rb
@@ -1,5 +1,6 @@
 require 'dotenv'
 require 'gmail'
+require 'fileutils'
 
 Dotenv.load
 
@@ -17,7 +18,7 @@ EMAIL_REGEX = /\[((\w+\s*)+)\]/
 
 	FileUtils.mkdir_p("data/pdf")
 FOLDER = File.join(Dir.pwd, 'data')
-FOLDER = File.join(Dir.pwd, 'data/pdf')
+PDF_FOLDER = File.join(Dir.pwd, 'data/pdf')
 
 
 def parse_subject(subject)
@@ -29,6 +30,10 @@ def parse_subject(subject)
 	exam, subject, code = parse_sub.map {|x| x[0].strip.downcase}
 	exam = exam.match(/^(mid|end)/).to_s
 	year = parse_sub[0][1]
+
+	# remove all the spaces from subject code
+	# CS 30002 -> CS30002
+	code = code.split.join.upcase
 
 	return {:exam => exam,
 		:subject => subject,
@@ -42,12 +47,13 @@ end
 
 def create_filename(parsed_subject, original_filename)
 	# assume that the default semester is mid-spring-2016
-	return "#{parsed_subject.exam}-spring-#{year}-#{parsed_subject.subject.uppercase}.#{original_filename.split(".")[-1]}"
+	return "#{parsed_subject[:exam]}-spring-#{parsed_subject[:year]}-#{parsed_subject[:code].upcase}.#{original_filename.split(".")[-1]}"
 end
 
 def create_unique_filename(parsed_subject, original_filename, file_contents)
 	# assume that the default semester is mid-spring-2016
-	return "#{parsed_subject.exam}-spring-#{year}-#{parsed_subject.subject.uppercase}-#{Digest::SHA256.hexdigest(file_contents)[0,10]}.#{original_filename.split(".")[-1]}"
+	non_unique_filename = create_filename(parsed_subject, original_filename)
+	return "#{non_unique_filename.split('.')[0]}-#{Digest::SHA256.hexdigest(file_contents)[0,10]}.#{non_unique_filename.split('.')[-1]}"
 end
 
 gmail.inbox.emails(:to => 'metakgp-qp@googlegroups.com', :after => DATE_AFTER) do |email|
@@ -81,14 +87,14 @@ gmail.inbox.emails(:to => 'metakgp-qp@googlegroups.com', :after => DATE_AFTER) d
 			if email.message.attachments.length == 1 and email.message.attachments[0].filename.split(".")[-1] == "pdf"
 				# save this PDF as the proper file name
 				file = email.message.attachments[0]
-				new_filename = create_unique_filename(parsed_sub, file.filename)
+				new_filename = create_unique_filename(parsed_sub, file.filename, file.body.decoded.to_s)
 				File.write(File.join(PDF_FOLDER, new_filename), file.body.decoded)
 			else
 				# the general case
 				# get the filename and append it with the first 10
 				# characters of the sha256 hash to ensure uniqueness
-				new_filename = create_unique_filename(parsed_sub, f.filename, f.body.decoded.to_s)
 				email.message.attachments.each do |f|
+					new_filename = create_unique_filename(parsed_sub, f.filename, f.body.decoded.to_s)
 					File.write(File.join(FOLDER, new_filename), f.body.decoded)
 				end
 			end
